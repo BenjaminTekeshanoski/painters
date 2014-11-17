cac1737c22b1276ae5a63f0ca07264805f9eb7da
aydanh <aydanh@itu.edu.tr>
2014-11-17 19:37:17 +0200

    Commented out database operations which uses local file on my computer

--- src/tr/edu/itu/cs/db/WicketApplication.java
+++ src/tr/edu/itu/cs/db/WicketApplication.java
@@ -3,7 +3,8 @@ package tr.edu.itu.cs.db;
 import org.apache.wicket.protocol.http.WebApplication;
 
 import tr.edu.itu.cs.hca.IMuseumCollection;
-import tr.edu.itu.cs.hca.MuseumCollectionJDBC;
+import tr.edu.itu.cs.hca.Museum;
+import tr.edu.itu.cs.hca.MuseumCollection;
 
 
 public class WicketApplication extends WebApplication {
@@ -12,8 +13,10 @@ public class WicketApplication extends WebApplication {
     @Override
     public void init() {
         super.init();
-        this._collection = new MuseumCollectionJDBC(
-                "C:/Users/HasanCan/Documents/itucsdb.sqlite");
+        /*
+         * this._collection = new MuseumCollectionJDBC(
+         * "C:/Users/HasanCan/Documents/itucsdb.sqlite");
+         */
     }
 
     @Override
@@ -21,15 +24,16 @@ public class WicketApplication extends WebApplication {
         return HomePage.class;
     }
 
-    /*
-     * public WicketApplication() { this.collection = new MuseumCollection();
-     * Museum museum1 = new Museum("Louvre Museum",
-     * "The world's most visited museum.", "Paris", 1973);
-     * collection.addMuseum(museum1); Museum museum2 = new
-     * Museum("Museum of Modern Art",
-     * "Most influential museum of modern art in the world.", "New York", 1929);
-     * collection.addMuseum(museum2); }
-     */
+    public WicketApplication() {
+        this._collection = new MuseumCollection();
+        Museum museum1 = new Museum("Louvre Museum",
+                "The world's most visited museum.", "Paris", 1973);
+        _collection.addMuseum(museum1);
+        Museum museum2 = new Museum("Museum of Modern Art",
+                "Most influential museum of modern art in the world.",
+                "New York", 1929);
+        _collection.addMuseum(museum2);
+    }
 
     public IMuseumCollection getCollection() {
         return this._collection;
