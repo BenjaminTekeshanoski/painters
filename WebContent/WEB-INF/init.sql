3afbed11f9043f6d91b9583171cd1a4428a4f978
aydanh <aydanh@itu.edu.tr>
2014-11-29 08:18:41 +0200

    Added rows for my tables

--- WebContent/WEB-INF/init.sql
+++ WebContent/WEB-INF/init.sql
@@ -17,7 +17,12 @@ CREATE TABLE YOURPAINTING(id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(20)
 
 INSERT INTO MUSEUM(name, year, location, desc) VALUES("Louvre Museum", 1973, "Paris", "The world's most visited museum");
 INSERT INTO MUSEUM(name, year, location, desc) VALUES("Museum of Modern Art", 1929, "New York", "Most influential museum of modern art in the world");
+INSERT INTO MUSEUM(name, year, location, desc) VALUES("National Gallery", 1824, "London", "It houses a collection of over 2,300 paintings dating from the mid-13th century to 1900");
+INSERT INTO MUSEUM(name, year, location, desc) VALUES("The Mauritshuis", 1822, "The Hauge", "The museum houses the Royal Cabinet of Paintings which consists of 841 objects");
+INSERT INTO MUSEUM(name, year, location, desc) VALUES("Rijksmuseum", 1800, "Amsterdam", "Netherlands national museum dedicated to arts and history");
+INSERT INTO MUSEUM(name, year, location, desc) VALUES("Museo Reina Sofia", 1992, "Madrid", "Spain's national museum of 20th-century art");
 INSERT INTO LOCATION(name, city, desc) VALUES("The Sistine Chapel", "Vatican City", "The Sistine Chapel is a large and renowned chapel in the Apostolic Palace, the official residence of the Pope" );
+INSERT INTO LOCATION(name, city, desc) VALUES("Santa Maria delle Grazie Monastery", "Milan", "Santa Maria delle Grazie is a church and Dominican convent in Milan, northern Italy" );
 INSERT INTO PAINTING(name, year, location, desc) VALUES("Mona Lisa"				, 1519, "Leonardo da Vinci" , "The Mona Lisa (Monna Lisa or La Gioconda in Italian; La Joconde in French) is a half-length portrait of a woman " );
 INSERT INTO PAINTING(name, year, location, desc) VALUES("Girl with a Mandolin"	, 1910, "Pablo Picasso" ,"...");
 INSERT INTO IDIOM(name, year, location, desc) VALUES("Girl with a Mandolin"	, 1910, "Pablo Picasso" ,"...");
