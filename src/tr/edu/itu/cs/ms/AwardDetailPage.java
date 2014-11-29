e734ec56f12244a3a35e7b13a8951301818ff7e7
ms <acer@160.75.202.6>
2014-11-29 23:12:44 +0200

    connection to paintings

--- src/tr/edu/itu/cs/ms/AwardDetailPage.java
+++ src/tr/edu/itu/cs/ms/AwardDetailPage.java
@@ -1,9 +1,15 @@
 package tr.edu.itu.cs.ms;
 
+import java.util.List;
+
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.PropertyListView;
 
 import tr.edu.itu.cs.db.BasePage;
+import tr.edu.tr.cs.sh.Painting;
+import tr.edu.tr.cs.sh.PaintingDetailPageLink;
 
 
 public final class AwardDetailPage extends BasePage {
@@ -14,7 +20,23 @@ public final class AwardDetailPage extends BasePage {
 
         this.add(new Label("name", aAward.getName()));
         this.add(new Label("owner", aAward.getOwner()));
-        this.add(new Label("year", Integer.toString(aAward.getYear())));
+        this.add(new Label("year", aAward.getYear()));
+
+        PaintingListGeneratorJDBC listgenerator = new PaintingListGeneratorJDBC();
+        List<Painting> paintings = listgenerator
+                .getPaintings(_award.getOwner());
+        PropertyListView paintingListView = new PropertyListView(
+                "painting_list", paintings) {
+            @Override
+            protected void populateItem(ListItem item) {
+                Painting painting = (Painting) item.getModelObject();
+                PaintingDetailPageLink paintingLink = new PaintingDetailPageLink(
+                        "painting_link", painting);
+                paintingLink.add(new Label("pname", painting.getName()));
+                item.add(paintingLink);
+            }
+        };
+        this.add(paintingListView);
 
         Link editLink = new Link("edit_link") {
             @Override
